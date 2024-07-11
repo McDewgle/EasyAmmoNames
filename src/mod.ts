a697e224334881035e2eb78e7c93a83b73df1f8b
mBarneto <4347791+m-barneto@users.noreply.github.com>
2024-07-11 11:55:08 -0400

    Detecting user's locale automatically! Also changed tab style

--- src/mod.ts
+++ src/mod.ts
@@ -3,37 +3,32 @@ import { DependencyContainer } from "tsyringe";
 import { IPostDBLoadMod } from "@spt/models/external/IPostDBLoadMod";
 import { ILogger } from "@spt/models/spt/utils/ILogger";
 import { DatabaseServer } from "@spt/servers/DatabaseServer";
+import { LocaleService } from "@spt/services/LocaleService";
 
-class EasyAmmoNames implements IPostDBLoadMod
-{
+class EasyAmmoNames implements IPostDBLoadMod {
     private modConfig = require("../config/config.json");
 
-    postDBLoad(container: DependencyContainer): void
-    {
+    postDBLoad(container: DependencyContainer): void {
         const logger = container.resolve<ILogger>("WinstonLogger");
         const databaseServer = container.resolve<DatabaseServer>("DatabaseServer");
+        const localeService = container.resolve<LocaleService>("LocaleService");
         const itemDatabase = databaseServer.getTables().templates.items;
-        const localeDatabase = databaseServer.getTables().locales.global[this.modConfig["locale"]];
+        const localeDatabase = localeService.getLocaleDb();
 
-        for (const itemTpl in this.modConfig["items"])
-        {
+        for (const itemTpl in this.modConfig["items"]) {
             const itemInfo = this.modConfig["items"][itemTpl];
             
-            if (itemTpl in itemDatabase)
-            {
+            if (itemTpl in itemDatabase) {
                 const itemName = itemTpl + " Name";
                 const itemShortName = itemTpl + " ShortName";
                 const itemDescription = itemTpl + " Description";
 
-                if (itemInfo["Name"] != "" && itemName in localeDatabase)
-                {
+                if (itemInfo["Name"] != "" && itemName in localeDatabase) {
                     localeDatabase[itemName] = itemInfo["Name"];
                 }
 
-                if (itemInfo["ShortName"] != "" && itemShortName in localeDatabase)
-                {
-                    if (itemInfo["ShortName"].length > 9)
-                    {
+                if (itemInfo["ShortName"] != "" && itemShortName in localeDatabase) {
+                    if (itemInfo["ShortName"].length > 9) {
                         logger.error("Provided shortname was too long! Shortnames have a maximum of 9 characters.");
                         logger.error(`Trimming ${itemInfo["ShortName"]} to ${itemInfo["ShortName"].substring(0, 9)}`);
                         itemInfo["ShortName"] = itemInfo["ShortName"].substring(0, 9);
@@ -42,8 +37,7 @@ class EasyAmmoNames implements IPostDBLoadMod
                     localeDatabase[itemShortName] = itemInfo["ShortName"];
                 }
 
-                if (itemInfo["Description"] != "" && itemDescription in localeDatabase)
-                {
+                if (itemInfo["Description"] != "" && itemDescription in localeDatabase) {
                     localeDatabase[itemDescription] = itemInfo["Description"];
                 }
             }
